ee756cdfca6192da9573c84a8d130b87b821c776
yla766 <yla766@sfu.ca>
2025-10-05 01:57:55 -0700

    mapped scene changing function to menu buttons

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -1,14 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.SceneManagement;
 public class MainMenu : MonoBehaviour
 {
     [SerializeField] string menuScene = "Menu";
     [SerializeField] string DrawingScene   = "SampleScene";
 
-    public void GoToAR()   => SceneManager.LoadScene(arScene);
-    public void GoToMenu() => SceneManager.LoadScene(menuScene);
+    public void GoToAR() {
+        SceneManager.LoadScene(DrawingScene);
+    }
+    public void GoToBuildModel() {
+        SceneManager.LoadScene(menuScene);
+    } 
 
     // Optional: a generic helper you can call from any button with a parameter
     public void LoadByName(string sceneName) => SceneManager.LoadScene(sceneName);
